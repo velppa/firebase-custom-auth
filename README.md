3b8369fb469f9f2e4874ea37e8543539efe01d79
Pavel Popov <pavelpopov@outlook.com>
2016-07-24 13:26:45 +0300

    Turn example to a package

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Firebase custom authorisation for Service Accounts in Go
 
-This is an example of obtaining authorisation token for Service Account that can be used for Firebase authentication.
+This package implements obtaining authentication token for Service Account that can be used for [Firebase].
+
+## Installation
+
+  go get github.com/schmooser/firebase-custom-auth
+
+## Usage
+
+1. Create Service Account, get keyfile from the Console
+2. Use this keyfile to obtain auth token:
+
+       token, err := auth.GetToken("keyfile.json")
+       if err != nil {
+         log.Fatalf("Failed to receive token: %s", err)
+       }
+       log.Printf("Token: %+v", token)
 
 ## Dependencies
 
@@ -10,3 +25,5 @@ This is an example of obtaining authorisation token for Service Account that can
 * [Firebase Database REST API User Authentication](https://firebase.google.com/docs/reference/rest/database/user-auth)
 * [Google Identity Platform - Using OAuth 2.0 for Server to Server Applications](https://developers.google.com/identity/protocols/OAuth2ServiceAccount#authorizingrequests)
 - Inspiration - [appengine-firebase-custom-auth](https://github.com/k2wanko-sandbox/appengine-firebase-custom-auth)
+
+[Firebase]: https://firebase.google.com
--- auth.go
+++ auth.go
@@ -1,9 +1,11 @@
-package main
+// Package auth implements obtaining authentication token for Service Account
+// that can be used for Firebase (https://firebase.google.com).
+package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -12,23 +14,23 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-// GIPClaims sets the JWT claims for Google Identity Platform.
-type GIPClaims struct {
+// gipClaims sets the JWT claims for Google Identity Platform.
+type gipClaims struct {
 	Scope string `json:"scope"`
 	jwt.StandardClaims
 }
 
-// FirebaseScopes defines scope for Firebase accounts defined in Google Identity Platform.
+// firebaseScopes defines scope for Firebase accounts defined in Google Identity Platform.
 var FirebaseScopes = []string{
 	"https://www.googleapis.com/auth/userinfo.email",
 	"https://www.googleapis.com/auth/firebase.database",
 }
 
-// GIPAuthURL sets Google Identity Platform auth url.
-const GIPAuthURL = "https://www.googleapis.com/oauth2/v4/token"
+// gipAuthURL sets Google Identity Platform auth url.
+const gipAuthURL = "https://www.googleapis.com/oauth2/v4/token"
 
-// ServiceAccount represents contents of service account keyfile.
-type ServiceAccount struct {
+// serviceAccount represents contents of service account keyfile.
+type serviceAccount struct {
 	Type                    string `json:"type"`
 	ProjectID               string `json:"project_id"`
 	PrivateKeyID            string `json:"private_key_id"`
@@ -41,26 +43,33 @@ type ServiceAccount struct {
 	ClientX509CertURL       string `json:"client_x509_cert_url"`
 }
 
-func main() {
+// Token is a Google Identity Platform auth token.
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+}
 
-	keyfileName := "keyfile.json"
-	keyfile, err := ioutil.ReadFile(keyfileName)
+// GetToken returns Auth Token
+func GetToken(keyfile string) (*Token, error) {
+
+	keyfileData, err := ioutil.ReadFile(keyfile)
 	if err != nil {
-		log.Fatalf("Can't read keyfile: %s", err)
+		return nil, fmt.Errorf("Can't read keyfile: %s", err)
 	}
 
-	var account ServiceAccount
-	if err := json.Unmarshal(keyfile, &account); err != nil {
-		log.Fatalf("Can't unmarshal keyfile: %s", err)
+	var account serviceAccount
+	if err := json.Unmarshal(keyfileData, &account); err != nil {
+		return nil, fmt.Errorf("Can't unmarshal keyfile: %s", err)
 	}
 
 	// Create the Claims
 	now := time.Now().Unix()
-	claims := GIPClaims{
+	claims := gipClaims{
 		Scope: strings.Join(FirebaseScopes, " "),
 		StandardClaims: jwt.StandardClaims{
 			Issuer:    account.ClientEmail,
-			Audience:  GIPAuthURL,
+			Audience:  gipAuthURL,
 			IssuedAt:  now,
 			ExpiresAt: now + (60 * 60),
 			Subject:   account.ClientEmail,
@@ -74,29 +83,40 @@ func main() {
 	// Reading privateKey contents
 	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(account.PrivateKey))
 	if err != nil {
-		log.Fatalf("Parsing RSA private key failed: %s", err)
+		return nil, fmt.Errorf("Parsing RSA private key failed: %s", err)
 	}
 
 	// Sign and get the complete encoded token as a string using the secret
 	tokenString, err := token.SignedString(privateKey)
 	if err != nil {
-		log.Fatalf("Creating jwt token string failed: %s", err)
+		return nil, fmt.Errorf("Creating jwt token string failed: %s", err)
 	}
 
 	// doing POST request to get access token
-	resp, err := http.PostForm(GIPAuthURL, url.Values{
+	resp, err := http.PostForm(gipAuthURL, url.Values{
 		"grant_type": {"urn:ietf:params:oauth:grant-type:jwt-bearer"},
 		"assertion":  {tokenString},
 	})
 	if err != nil {
-		log.Fatalf("Sending post request to Google Identity Platform failed: %s", err)
+		return nil, fmt.Errorf("Sending post request to Google Identity Platform failed: %s", err)
 	}
-	log.Printf("Response: %+v\n", resp)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalf("Read response body failed: %s", err)
+		return nil, fmt.Errorf("Read response body failed: %s", err)
 	}
 
-	log.Printf("Body: %s", string(body))
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("%+v", struct {
+			StatusCode int
+			Body       string
+		}{resp.StatusCode, string(body)})
+	}
+
+	authToken := Token{}
+	if err := json.Unmarshal(body, &authToken); err != nil {
+		return nil, fmt.Errorf("Can't unmarshal auth token from body: %s", err)
+	}
+
+	return &authToken, nil
 }
